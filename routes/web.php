aaf83b5cfa634f1e5596a46df13bf9e2a4f8ede0
soulmanzen <soulmanzen@gmail.com>
2017-05-18 19:26:31 +0300

    second commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+Route::get('/', 'TasksController@index');
+
+Auth::routes();
+
+Route::get('/task','TasksController@add');
+Route::post('/task','TasksController@create');
+
+Route::get('/task/{task}','TasksController@edit');
+Route::post('/task/{task}','TasksController@update');
